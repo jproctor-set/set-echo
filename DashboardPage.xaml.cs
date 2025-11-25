8e3187ffc742f76065a0a0fe14aa7e9cb84b4dcd
jproctor <jproctor@5680-2024-ME2>
2025-11-25 08:27:46 -0600

    Add project files.

---
+++ DashboardPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SETEcho
+{
+    /// <summary>
+    /// Interaction logic for DashboardPage.xaml
+    /// </summary>
+    public partial class DashboardPage : Page
+    {
+        public DashboardPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btn_PO_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService?.Navigate(new PurchaseOrderPage());
+
+        }
+    }
+}
